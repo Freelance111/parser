c41dc84a37e61f9ee9c3fc93078261216bf8f076
Maxwell012 <kyrbanovdavid2004@gmail.com>
2022-10-25 17:20:43 +0200

    added block try except for connection with database

--- postgresql.py
+++ postgresql.py
@@ -5,7 +5,11 @@ import time
 
 class Database:
     def __init__(self):
-        self.connection = psycopg2.connect(**config)
+        try:
+            self.connection = psycopg2.connect(**config)
+            print('\tSuccessful connection')
+        except Exception as ex:
+            print(f'\tConnection refused\n\n {ex}')
 
     async def add_data(self, data, url):
         sql = "UPDATE information SET site=%s, title=%s, meta_description=%s," \
